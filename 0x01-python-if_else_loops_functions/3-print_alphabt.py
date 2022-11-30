5265d0c6e906914d41a0f8524baa3a0eef0f3d6e
David Egwuatu <davidegwuatu2@gmail.com>
2022-11-30 03:50:59 +0100

    Done task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z')+1):
-	if chr(i) is 'e' or chr(i) is 'q':
-	    continue
-	print("{:c}".format(i), end="")
+    if chr(i) == 'e' or chr(i) == 'q':
+        continue
+    print("{:c}".format(i), end="")
