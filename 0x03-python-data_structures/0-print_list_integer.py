eb54b089ef0c2967dd9579ebf3cf6b82f49aea10
David Egwuatu <davidegwuatu2@gmail.com>
2022-12-05 04:41:13 +0100

    added some requirements

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,4 +3,4 @@ def print_list_integer(my_list=[]):
     if not len(my_list):
         return
     for x in range(0, len(my_list)):
-        print(my_list[x])
+        print("{:d}".format(my_list[x]))
