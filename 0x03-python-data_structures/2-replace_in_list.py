705c9a1b9de2210674ccfe852a4271acc5683061
David Egwuatu <davidegwuatu2@gmail.com>
2022-12-05 05:06:31 +0100

    Fix changes

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,5 +4,7 @@ def replace_in_list(my_list, idx, element):
         return
     if idx >= len(my_list):
         return
+    new_list = []
     my_list[idx] = element
-    return my_list
+    new_list = my_list
+    return new_list
