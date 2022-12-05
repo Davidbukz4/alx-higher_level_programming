acf930f19ad57cc58533c83f6067d1e336644394
David Egwuatu <davidegwuatu2@gmail.com>
2022-12-05 07:35:21 +0100

    Done task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if not my_string:
+        return
+    new_str = ""
+    for x in my_string:
+        if x != 'c' and x != 'C':
+            new_str = new_str + x
+    return new_str
