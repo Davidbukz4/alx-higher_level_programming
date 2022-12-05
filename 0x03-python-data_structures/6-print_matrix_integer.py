f0f8198356dab42437a4ea76f5aa92d9ca059fe5
David Egwuatu <davidegwuatu2@gmail.com>
2022-12-05 10:05:06 +0100

    fixed pycodestyle error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            if matrix[i][j] !=  matrix[i][-1]:
+            if matrix[i][j] !  matrix[i][-1]:
                 print("{:d}".format(matrix[i][j]), end=" ")
             else:
                 print("{:d}".format(matrix[i][j]), end="")
