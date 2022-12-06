39345f26891bff42d4d76145e9f03f62ee1c2e1e
David Egwuatu <davidegwuatu2@gmail.com>
2022-12-06 13:53:14 +0100

    Some changes

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -10,13 +10,16 @@
 
 void print_python_list_info(PyObject *p)
 {
-	int i = 0, j = Py_SIZE(p);
+	unsigned int i = 0, j = Py_SIZE(p);
 	PyObject *item;
 
+if (PyList_Check(p))
+{    
 	printf("[*] Size of the Python List = %ld\n", j);
 	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 	for (i = 0; i < j; i++)
 	{
 		printf("Element %d: %s\n", i, Py_TYPE(item)->tp_name);
 	}
+}
 }
