f46be21d0e87cfaa9f96d9e5e831238a699de47f
David Egwuatu <davidegwuatu2@gmail.com>
2022-12-07 21:36:10 +0100

    Done task 10.

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionary is {}:
         return None
     highest_value = max(a_dictionary.values())
     for key, value in a_dictionary.items():
