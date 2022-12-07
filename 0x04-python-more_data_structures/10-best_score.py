0468cf040c1f53059f98d3cadb4e6e55c87d1103
David Egwuatu <davidegwuatu2@gmail.com>
2022-12-07 21:31:45 +0100

    Done task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    highest_value = max(a_dictionary.values())
+    for key, value in a_dictionary.items():
+        if a_dictionary.get(key) == highest_value:
+            return key
