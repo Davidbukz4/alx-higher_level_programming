b80634b99cdabd5df0f05a9520405e74b9ab0770
David Egwuatu <davidegwuatu2@gmail.com>
2022-12-07 17:09:50 +0100

    Added a requirement in task 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,7 +3,7 @@
 def common_elements(set_1, set_2):
     if not set_1 or not set_2:
         return None
-    new_list = []
+    new_list = set()
 # & can also be used for intersection
     new_list = set_1.intersection(set_2)
     return new_list
