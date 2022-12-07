fab4c89343622fc49304725627d2d08e42e61bed
David Egwuatu <davidegwuatu2@gmail.com>
2022-12-07 17:07:01 +0100

    Done task 3

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    if not set_1 or not set_2:
+        return None
+    new_list = []
+# & can also be used for intersection
+    new_list = set_1.intersection(set_2)
+    return new_list
