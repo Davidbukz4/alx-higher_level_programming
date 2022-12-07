a92940befb7c1e1f33c0cd9c4c33aa9b196d6d3e
David Egwuatu <davidegwuatu2@gmail.com>
2022-12-07 20:53:21 +0100

    Done task 8

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def simple_delete(a_dictionary, key=""):
+    if not key or not a_dictionary.get(key):
+        return a_dictionary
+    del a_dictionary[key]
+    return a_dictionary
