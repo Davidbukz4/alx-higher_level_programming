81a5c895abd133f84d89ce22ca72cca2bf6288d6
David Egwuatu <davidegwuatu2@gmail.com>
2022-12-07 16:41:39 +0100

    Done task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    if my_list is None:
+        return None
+    new_list = []
+    new_list = list(map((lambda x: x if x != search else replace), my_list))
+
+    return new_list
