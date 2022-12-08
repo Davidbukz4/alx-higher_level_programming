e9be1f5d3b0d7b0111c43c278603ff1a4a9880f7
David Egwuatu <davidegwuatu2@gmail.com>
2022-12-08 04:21:32 +0100

    Fixed pycodestyle error on task 14

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map((lambda list_2d: list(map((lambda x: x ** 2), list_2d))), matrix))
+    return list(map((lambda sub: list(map((lambda x: x ** 2), sub))), matrix))
