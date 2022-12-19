0c0f471ad7d2925e97d5b03950ad87ebeed92e03
David Egwuatu <davidegwuatu2@gmail.com>
2022-12-19 12:58:54 +0100

    fixed pycodestyle error on task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -12,6 +12,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
         print("")
     except IndexError:
-            print("")
-            p_count = count
+        print("")
+        p_count = count
     return p_count
