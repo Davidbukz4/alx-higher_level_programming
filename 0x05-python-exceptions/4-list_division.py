ace64203020b05436a833b73c3347150e1e1f0b0
David Egwuatu <davidegwuatu2@gmail.com>
2022-12-19 14:47:40 +0100

    fixed error on task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,5 +16,6 @@ def list_division(my_list_1, my_list_2, list_length):
             res = 0
         except Exception:
             res = 0
-        new_list.append(res)
+        finally: 
+            new_list.append(res)
     return new_list
