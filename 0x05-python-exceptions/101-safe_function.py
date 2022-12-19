b46e58f81a513f9c3c65722d26b37cbe06c69681
David Egwuatu <davidegwuatu2@gmail.com>
2022-12-19 15:53:46 +0100

    Fixed an error on task 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,12 @@ def safe_function(fct, *args):
     try:
         a, b = args
         res = fct(a, b)
+    except ZeroDivisionError as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        res = None
+    except IndexError as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        res = None
     except Exception as err:
         sys.stderr.write("Exception: {}\n".format(err))
         res = None
