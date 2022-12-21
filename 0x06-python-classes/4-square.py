17b140e0b278b1d2b44f55c1d0cdb96109f8b918
David Egwuatu <davidegwuatu2@gmail.com>
2022-12-21 04:11:04 +0100

    Done task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This module contains a class that defines a square"""
+
+
+class Square():
+    """Defines a square"""
+    def __init__(self, size=0):
+        """Initializes a new square
+        Args:
+            size (int): size of square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieves the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Sets the size"""
+        if isinstance(size, int) and size >= 0:
+            self.__size = size
+        elif not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif (size < 0):
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size ** 2
