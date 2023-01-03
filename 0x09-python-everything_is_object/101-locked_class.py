7f90846d83ca20a0477d5fd49bc3e93edc6595df
David Egwuatu <davidegwuatu2@gmail.com>
2023-01-03 16:42:35 +0100

    fixed pycodestyle error

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,5 +7,6 @@ This is a lockedClass module
 class LockedClass():
     """prevents the user from dynamically creating new instance attributes"""
     __slots__ = ['first_name']
+
     def __inti__(self):
         pass
