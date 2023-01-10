ee8584b54eb0d96d1ab336fb1e49a43742f1169e
David Egwuatu <davidegwuatu2@gmail.com>
2023-01-10 05:09:35 +0100

    fixed some errors

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,6 +4,7 @@ Defines a function that returns list of available
 attribute and methods of an object
 """
 
+
 def lookup(obj):
     """Returns the list of attributes and methods of an object"""
     return dir(obj)
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,11 +4,15 @@ Defines a function that checks if an object is exactly an instance
 of the specified class
 """
 
+
 def is_same_class(obj, a_class):
     """Returns True if the object is exactly an instance
     of the specified class
+    Args:
+        obj (any): the object to check
+        a_class (type): the class to compare the type of obj to
     """
-	if type(obj) == a_class:
-		return True
-	else:
-		return False
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
