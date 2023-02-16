d1ecdc937d0dcd2a7ab701474a24b3cbc7e5bc7c
David Egwuatu <davidegwuatu2@gmail.com>
2023-02-16 05:38:12 +0100

    done task 0-16

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,5 @@
+-- a script that lists all the cities of California that can be
+-- found in the database hbtn_0d_usa
+SELECT cities.id, cities.name 
+FROM states, cities
+WHERE cities.state_id = states.state_id AND states.name = "California";
