615993aeeb94a27a6552bc3e0eba4df1c7f3d855
David Egwuatu <davidegwuatu2@gmail.com>
2023-03-18 00:48:26 +0100

    fixed task 2 errors

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,27 +9,26 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    if (len(sys.argv) == 5):
-        uname = sys.argv[1]
-        pwd = sys.argv[2]
-        dbName = sys.argv[3]
-        # connect to database
-        db = MySQLdb.connect(host='localhost', user=uname, passwd=pwd,
-                             db=dbName, port=3306)
+    uname = sys.argv[1]
+    pwd = sys.argv[2]
+    dbName = sys.argv[3]
+    # connect to database
+    db = MySQLdb.connect(host='localhost', user=uname, passwd=pwd,
+                         db=dbName, port=3306)
 
-        # get a cursor
-        query = db.cursor()
+    # get a cursor
+    query = db.cursor()
 
-        # execute MySQL queries
-        query.execute("SELECT * FROM states WHERE name='{}' ORDER BY id ASC"
-                      .format(sys.argv[4]))
+    # execute MySQL queries
+    query.execute("SELECT * FROM states WHERE name='{}' ORDER BY id ASC"
+                  .format(sys.argv[4]))
 
-        # obtain query
-        data = query.fetchall()
-        for x in data:
-            print(x)
+    # obtain query
+    data = query.fetchall()
+    for x in data:
+        print(x)
 
-        # close the cursor
-        query.close()
-        # close the database
-        db.close()
+    # close the cursor
+    query.close()
+    # close the database
+    db.close()
