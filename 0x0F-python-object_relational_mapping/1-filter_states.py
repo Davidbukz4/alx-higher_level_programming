43af1ff6d3ee242d5bfc572f949879fea422b037
David Egwuatu <davidegwuatu2@gmail.com>
2023-03-17 23:46:41 +0100

    finished task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Accessing a database
+using MySQLdb
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    if (len(sys.argv) == 4):
+        uname = sys.argv[1]
+        pwd = sys.argv[2]
+        dbName = sys.argv[3]
+        # connect to database
+        db = MySQLdb.connect(host='localhost', user=uname, passwd=pwd,
+                             db=dbName, port=3306, charset="utf8")
+
+        # get a cursor
+        query = db.cursor()
+
+        # execute MySQL queries
+        query.execute("""SELECT * FROM states WHERE name
+                      LIKE 'N%' ORDER BY states.id ASC""")
+
+        # obtain query
+        data = query.fetchall()
+        for x in data:
+            print(x)
+
+        # close the cursor
+        query.close()
+        # close the database
+        db.close()
