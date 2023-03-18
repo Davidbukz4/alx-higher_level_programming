481cbbdbbec8428e3cc99cbc7bd59cfc35b5b550
David Egwuatu <davidegwuatu2@gmail.com>
2023-03-18 15:43:18 +0100

    fixed task 14 error(vi)

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -7,16 +7,29 @@ import sys
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
 from model_state import Base, State
-from model_city import City
 
 if __name__ == "__main__":
     # creates an engine that connects to the database
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+
+    # take the imported base class and create them in the database
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+
+    # create a session maker
+    Session = sessionmaker(bind=engine)  # it returns a class
+
+    # make an instance of the session class
     session = Session()
-    for instance in (session.query(State.name, City.id, City.name)
-                     .filter(State.id == City.state_id)):
-        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2]) 
+
+    # get data in order, but return the first
+    data_out = session.query(State.name, City.id, City.name)\
+                      .filter(State.id == City.state_id).order_by(City.id)
+    for x in data_out:
+        session.delete(x)
+
+    # commit to database
+    session.commit()
+
+    session.close()
