f59a1d14f9ca58a98379c2bc153d17484a3ecdbb
David Egwuatu <davidegwuatu2@gmail.com>
2023-03-18 11:19:47 +0100

    finished task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Lists all state object from the database
+"""
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    # create an engine that connects to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # take the imported base class and create them in the database
+    Base.metadata.create_all(engine)
+
+    # create a session maker
+    Session = sessionmaker(bind=engine)  # it returns a class
+
+    # make an instance of the Session class
+    session = Session()
+
+    # get data in order, returns the first record
+    data = session.query(State).order_by(State.id)
+    .filter(State.name.like('%a%'))
+    if not(data):
+        print('Nothing')
+    else:
+        print('{}: {}'.format(data.id, data.name))
+
+    # close the session
+    session.close()
