0998aa2c60af7249704ddc5283d8ce4c23689d7d
David Egwuatu <davidegwuatu2@gmail.com>
2023-03-18 09:24:09 +0100

    added model module

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+State model
+"""
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import creative_engine, Column, Integer, String
+
+# base class to inherit from
+Base = declarative_base() # it returns a class
+
+class State(Base):
+    """ Class to run in ORM """
+    __tablename__ = 'states'
+
+    id = Column(Integer, unique=True, primary_key=True,
+                autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
