6261da279ac7a2484f33606adae07e9271c744ee
David Egwuatu <davidegwuatu2@gmail.com>
2023-03-18 14:08:03 +0100

    finished task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Lists all state object from the database
+using sqlalchemy
+"""
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # creates an engine that connects to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    # take the imported base class and create them in the database
+    Base.metadata.create_all(engine)
+
+    # create a session maker
+    Session = sessionmaker(bind=engine)  # it returns a class
+
+    # make an instance of the session class
+    session = Session()
+
+    # get data in order, but return the first
+    data = session.query(State).filter(State.name==(sys.argv[4],))
+    if not(data.first()):
+        print('Not found')
+    else:
+        for x in data:
+            print("{}".format(x.id))
+    session.close()
