01fb45044dc259955324e6e18504fd406e2a1ae5
David Egwuatu <davidegwuatu2@gmail.com>
2023-03-18 10:22:26 +0100

    fixed task 8 error(vii)

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -8,7 +8,7 @@ from sqlalchemy import (create_engine)
 from model_state import Base, State
 
 if __name__ == "__main__":
-    # 
+    # creates an engine that connects to the database
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
