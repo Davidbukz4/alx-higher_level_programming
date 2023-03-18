23413283969db7cac51f6ee3b4f5d87193867f7f
David Egwuatu <davidegwuatu2@gmail.com>
2023-03-18 15:36:16 +0100

    fixed task 14 error(iii)

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -14,19 +14,9 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-
-    # take the imported base class and create them in the database
     Base.metadata.create_all(engine)
-
-    # create a session maker
-    Session = sessionmaker(bind=engine)  # it returns a class
-
-    # make an instance of the session class
+    Session = sessionmaker(bind=engine)
     session = Session()
-
-    # get data in order, but return the first
-    data_out = session.query(State.name, City.id, City.name).filter(State.id == City.state_id).order_by(City.id)
-    for x in data_out:
-        print('{}: ({}) {}'.format(x[0], x[1], x[2]))
-
-    session.close()
+    for instance in (session.query(State.name, City.id, City.name)
+                     .filter(State.id == City.state_id)):
+        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2]) 
