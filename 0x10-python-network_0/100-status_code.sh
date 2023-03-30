68523d18c4722edfacdab98ff6b38eb546e2130a
David Egwuatu <davidegwuatu2@gmail.com>
2023-03-30 19:00:39 +0000

    modified task 7

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to a url passed as first arg and displays status code of response
-curl -sI "$1" | grep 'HTTP/1.1' | cut -d' ' -f2 | tr -d '\n'
+curl -so /dev/null -w "%{http_code}" "$1"
