d28a63f36246ba3d553bb9e4b1719c10b95ad4c5
David Egwuatu <davidegwuatu2@gmail.com>
2023-03-30 15:46:34 +0000

    resolved task 5 errors(ii)

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a post request to a url passed as arg
-curl -s "$1" -X POST -d email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD"
