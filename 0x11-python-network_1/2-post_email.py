0d635f480fc9e93a9af113b70bc17ba5282b61c5
David Egwuatu <davidegwuatu2@gmail.com>
2023-03-31 18:47:47 +0000

    finished task 3

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A script that takes in a URL and an email, sends a post request to the 
+passed URL with the email as parameter and displays the body of the response
+"""
+
+if __name__ == '__main__':
+    import sys
+    import urllib.request
+    import urllib.parse
+
+
+    data = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(data).encode('utf-8')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as f:
+        body = f.read().decode('utf-8')
+        print(body)
