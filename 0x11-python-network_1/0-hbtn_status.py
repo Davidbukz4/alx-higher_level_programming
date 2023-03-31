0c91ee62867d3cb98a4dba4fb216f3b24f4baf4b
David Egwuatu <davidegwuatu2@gmail.com>
2023-03-31 16:56:08 +0000

    finished task 1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+What's my status?
+"""
+import urllib.request
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as f:
+    print('Body response:')
+    data = f.read()
+    print('\t - type: {}'.format(type(data)))
+    print('\t - content: {}'.format(data))
+    print('\t - utf8 content: {}'.format(data.decode('utf-8')))
