47cb8171b6c6b9e6ce65e3eecddfd73d08eecf7b
David Egwuatu <davidegwuatu2@gmail.com>
2023-03-31 17:19:46 +0000

    fixed task 1 error(iv)

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,12 +6,9 @@ if __name__ == '__main__':
     import urllib.request
 
     req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    try:
-        with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as f:
-            print('Body response:')
-            data = f.read()
-            print('\t - type: {}'.format(type(data)))
-            print('\t - content: {}'.format(data))
-            print('\t - utf8 content: {}'.format(data.decode('utf-8')))
-    except urllib.error.URLError as e:
-        print(e.reason)
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as f:
+        print('Body response:')
+        data = f.read()
+        print('\t - type: {}'.format(type(data)))
+        print('\t - content: {}'.format(data))
+        print('\t - utf8 content: {}'.format(data.decode('utf-8')))
