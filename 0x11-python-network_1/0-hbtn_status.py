e475a45f7d650463183ebe4cd739b59fafbbd6d0
David Egwuatu <davidegwuatu2@gmail.com>
2023-03-31 18:00:46 +0000

    modified task 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     import urllib.request
 
     req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as f:
+    with urllib.request.urlopen(req) as f:
         print('Body response:')
         data = f.read()
         print('\t- type: {}'.format(type(data)))
