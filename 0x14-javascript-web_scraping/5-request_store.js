e25a0b2cc13d5e4acb10d0b4704385cee7d7025b
David Egwuatu <davidegwuatu2@gmail.com>
2023-04-25 12:30:07 +0000

    added task 6

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filename = process.argv[3];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(filename, body, 'utf-8', () => {});
+  }
+});
